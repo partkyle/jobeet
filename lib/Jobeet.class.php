a7ee3d9f2802a63f17b64c9bb772315cf6eb60ea
Kyle Partridge <partkyle@gmail.com>
2011-10-01 14:38:06 -0700

    updated slugify

--- lib/Jobeet.class.php
+++ lib/Jobeet.class.php
@@ -7,8 +7,23 @@ class Jobeet
 {
   static public function slugify($text)
   {
-    $text = preg_replace('/\W+/', '-', $text);
-    $text = strtolower(trim($text, '-'));
+    // replace non letter or digits by -
+    $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+    // trim
+    $text = trim($text, '-');
+
+    // transliterate
+    if (function_exists('iconv'))
+    {
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+    }
+
+    // lowercase
+    $text = strtolower($text);
+
+    // remove unwanted characters
+    $text = preg_replace('~[^-\w]+~', '', $text);
 
     if (empty($text))
     {
