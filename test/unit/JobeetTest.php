801c7bb331e78f2e4c2162f56ee169f41390e896
Kyle Partridge <partkyle@gmail.com>
2011-10-01 14:26:07 -0700

    added unit test for slugify

---
+++ test/unit/JobeetTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once dirname(__FILE__).'/../bootstrap/unit.php';
+ 
+$t = new lime_test(6, new lime_output_color());
+
+$t->is(Jobeet::slugify('Sensio'), 'sensio');
+$t->is(Jobeet::slugify('sensio labs'), 'sensio-labs');
+$t->is(Jobeet::slugify('sensio   labs'), 'sensio-labs');
+$t->is(Jobeet::slugify('paris,france'), 'paris-france');
+$t->is(Jobeet::slugify('  sensio'), 'sensio');
+$t->is(Jobeet::slugify('sensio  '), 'sensio');
