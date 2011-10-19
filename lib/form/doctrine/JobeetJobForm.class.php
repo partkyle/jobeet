315df6f51e920a5884bfa69e70e54ed8471f62a6
Kyle Partridge <partkyle@gmail.com>
2011-10-18 23:17:55 -0700

    added form object

--- lib/form/doctrine/JobeetJobForm.class.php
+++ lib/form/doctrine/JobeetJobForm.class.php
@@ -12,5 +12,51 @@ class JobeetJobForm extends BaseJobeetJobForm
 {
   public function configure()
   {
+    $this->useFields(array(
+      'category_id',
+      'type',
+      'company',
+      'logo',
+      'url',
+      'position',
+      'location',
+      'description',
+      'how_to_apply',
+      'token',
+      'is_public',
+      'email'
+    ));
+
+    $this->widgetSchema->setLabels(array(
+      'category_id' => 'Category',
+      'is_public' => 'Public?',
+      'how_to_apply' => 'How to apply?',
+    ));
+
+    $this->widgetSchema->setHelp('is_public', 'Whether the job can also be published on affiliate websites or not.');
+
+    $this->widgetSchema['type'] = new sfWidgetFormChoice(array(
+      'choices' => Doctrine_Core::getTable('JobeetJob')->getTypes(),
+      'expanded' => true,
+    ));
+
+    $this->validatorSchema['type'] = new sfValidatorChoice(array(
+      'choices' => Doctrine_Core::getTable('JobeetJob')->getTypes(),
+    ));
+
+    $this->widgetSchema['logo'] = new sfWidgetFormInputFile(array(
+      'label' => 'Company Logo',
+    ));
+
+    $this->validatorSchema['logo'] = new sfValidatorFile(array(
+      'required' => false,
+      'path' => sfConfig::get('sf_upload_dir').'/jobs',
+      'mime_types' => 'web_images',
+    ));
+    
+    $this->validatorSchema['email'] = new sfValidatorAnd(array(
+      $this->validatorSchema['email'],
+      new sfValidatorEmail(),
+    ));
   }
 }
