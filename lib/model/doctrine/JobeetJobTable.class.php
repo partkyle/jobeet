4f920f8da2431aa2a3f8ddde9fc7ef53fc5fefc8
Kyle Partridge <partkyle@gmail.com>
2011-10-18 23:04:13 -0700

    added types static array to the jobs table class

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -7,44 +7,55 @@
  */
 class JobeetJobTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object JobeetJobTable
-     */
-    public static function getInstance()
+  /**
+   * Returns an instance of this class.
+   *
+   * @return object JobeetJobTable
+   */
+  public static function getInstance()
+  {
+      return Doctrine_Core::getTable('JobeetJob');
+  }
+
+  static public $types = array(
+    'full-time' => 'Full time',
+    'part-time' => 'Part time',
+    'freelance' => 'Freelance',
+  );
+ 
+  public function getTypes()
+  {
+    return self::$types;
+  }
+
+  public function getActiveJobs(Doctrine_Query $q = null)
+  {
+    return $this->addActiveJobsQuery($q)->execute();
+  }
+
+  public function retrieveActiveJob(Doctrine_Query $q = null)
+  {
+    return $this->addActiveJobsQuery($q)->fetchOne();
+  }
+
+  public function countActiveJobs(Doctrine_Query $q = null)
+  {
+    return $this->addActiveJobsQuery($q)->count();
+  }
+
+  public function addActiveJobsQuery(Doctrine_Query $q = null)
+  {
+    if (is_null($q))
     {
-        return Doctrine_Core::getTable('JobeetJob');
+      $q = Doctrine_Query::create()
+        ->from('JobeetJob j');
     }
 
-    public function getActiveJobs(Doctrine_Query $q = null)
-    {
-      return $this->addActiveJobsQuery($q)->execute();
-    }
+    $alias = $q->getRootAlias();
 
-    public function retrieveActiveJob(Doctrine_Query $q = null)
-    {
-      return $this->addActiveJobsQuery($q)->fetchOne();
-    }
+    $q->andWhere($alias . '.expires_at > ?', date('Y-m-d H:i:s', time()))
+      ->addOrderBy($alias . '.created_at DESC');
 
-    public function countActiveJobs(Doctrine_Query $q = null)
-    {
-      return $this->addActiveJobsQuery($q)->count();
-    }
-
-    public function addActiveJobsQuery(Doctrine_Query $q = null)
-    {
-      if (is_null($q))
-      {
-        $q = Doctrine_Query::create()
-          ->from('JobeetJob j');
-      }
-
-      $alias = $q->getRootAlias();
-
-      $q->andWhere($alias . '.expires_at > ?', date('Y-m-d H:i:s', time()))
-        ->addOrderBy($alias . '.created_at DESC');
-
-      return $q;
-    }
+    return $q;
+  }
 }
